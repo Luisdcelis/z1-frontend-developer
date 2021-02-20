aa4811743ab0d5cbc8a17faf3e4f2f87693357d2
lui <luisdecelis3@gmail.com>
2021-02-20 21:42:48 +0100

    primera vista

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import  NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: "lightgray"}}>
+      <div style={{marginLeft: 500, marginRight: 500, backgroundColor: "white"}}>
+        <NavBar />
+
+        <div style={{textAlign: "center"}}>
+          <br/>
+          <h1>
+            Scan your ID
+          </h1>
+          <br/>
+          <div style={{fontSize: 22}}>
+            Take a picture. It may take time to validate your personal information
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <div>
+            <h1 style={{color: "rgb(43,0,118)", fontStyle: "italic", padding: 20, marginBottom: 0}} >
+                BankClient
+            </h1>
+            <hr style={{opacity: 0.4}} />
+        </div>
+            
+    )
+}
+
+export default NavBar;
