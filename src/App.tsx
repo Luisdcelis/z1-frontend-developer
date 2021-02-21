6ad47aa9fd4cb9104ce9bd403190684f6195f94a
lui <luisdecelis3@gmail.com>
2021-02-21 18:24:59 +0100

    terminando capturas

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,30 @@ import React, { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import TakePictureButton from "./components/TakePictureButton";
-import ModalCam from "./components/ModalCam/index"
+import ModalCam from "./components/ModalCam/index";
+import Desktop from "./assets/desktop.jpg";
 
 function App() {
   const [open, setOpen] = useState(false);
 
-  return (
-      open ? (
-        <ModalCam />
-      ) : (
-        <>
-          <NavBar />
-          <div style={{ textAlign: "center" }}>
-            <h1 style={{ margin: "40px 0" }}>Scan your ID</h1>
-            <div style={{ fontSize: 22, marginBottom: "20px" }}>
-              Take a picture. It may take time to validate your personal
-              information
-            </div>
-            <TakePictureButton openModal={() => setOpen(!open)} />
-          </div>
-        </>
-      )
+  return open ? (
+    <>
+      <img src={Desktop} alt="background" className="bg-image" />
+      <div className="modal-cam">
+        <ModalCam closeModal={() => setOpen(!open)} />
+      </div>
+    </>
+  ) : (
+    <>
+      <NavBar />
+      <div style={{ textAlign: "center" }}>
+        <h1 style={{ margin: "40px 0" }}>Scan your ID</h1>
+        <div style={{ fontSize: 22, marginBottom: "20px" }}>
+          Take a picture. It may take time to validate your personal information
+        </div>
+        <TakePictureButton openModal={() => setOpen(!open)} />
+      </div>
+    </>
   );
 }
 
--- src/components/ModalCam/index.tsx
+++ src/components/ModalCam/index.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Webcam from "react-webcam";
 import "./index.css";
 
-function ModalCam() {
+interface ModalCamProps {
+  closeModal: () => void;
+}
+
+function ModalCam({ closeModal }: ModalCamProps) {
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("Hello, World!");
+    }, 3000);
+  }, []);
+
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState(null);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Take picture</h1>
-      <p>Fit your ID card inside the frame</p>
-      <p>The picture will be taken automatically</p>
+      <h1 style={{ color: "white" }}>Take picture</h1>
+      <p style={{ color: "white" }}>Fit your ID card inside the frame</p>
+      <p style={{ color: "white" }}>The picture will be taken automatically</p>
       <div className="container">
-        <Webcam audio={false} style={{ borderRadius: "15px", height: "90%" }} />
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          style={{ borderRadius: "15px", height: "90%" }}
+        />
         <div className="borderCard" />
       </div>
+      <p>aqui va el tectto</p>
+      <button className="button-cancel" onClick={closeModal}>
+        CANCEL
+      </button>
     </div>
   );
 }
